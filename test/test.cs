8b4a338bee10410b27b8334cb173dfc49730288b
musmemur <timurkrivoscheev2005@gmail.com>
2023-10-07 13:36:06 +0300

    контрольная работа

---
+++ test/test.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {	
+			string s = "abcde";
+			string goal = "deabc";
+			Console.WriteLine(IsRotatedString(s, goal));
+		}	
+		static bool IsRotatedString(string s, string goal) 
+		{
+			s = s + s;
+			if (s.CountOf(goal) != -1)
+			{
+				return true;
+			}
+			return false;
+		}
+    }
+}
